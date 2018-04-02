10efea3b94aa097ccf0e20bd89f7f652fc356880
Swapnesh Chaubal <Swapnesh.Chaubal@gmail.com>
2018-04-01 21:50:47 -0700

    simple finch webserver with get endpoints

--- scala-projects/fb/build.sbt
+++ scala-projects/fb/build.sbt
@@ -1,13 +1,35 @@
-name := "hello"
+name := "campaigns-service"
 
 version := "1.0"
 
 scalaVersion := "2.11.1"
 
+lazy val json4sVersion = "3.5.3"
+
+// Facebook SDK
 libraryDependencies ++= Seq(
-    "com.facebook.ads.sdk" % "facebook-java-ads-sdk" % "2.11.1",
-    "org.json4s" %% "json4s-ext" % "3.6.0-M2",
-    "org.json4s" %% "json4s-jackson" % "3.6.0-M2",
-    "com.twitter" %% "bijection-core" % "0.9.5",
-    "com.twitter" %% "bijection-util" % "0.9.5"
+  "com.facebook.ads.sdk" % "facebook-java-ads-sdk" % "2.11.1"
+)
+
+// Json conversions
+libraryDependencies ++= Seq(
+  "org.json4s" %% "json4s-ext" % json4sVersion,
+  "org.json4s" %% "json4s-jackson" % json4sVersion,
+  "com.fasterxml.jackson.module" %% "jackson-module-scala" % "2.9.4"
+//  "org.json4s" %% "json4s-ext" % "3.6.0-M2",
+//  "org.json4s" %% "json4s-jackson" % "3.6.0-M2"
+)
+
+// Twitter libs for bijection/conversions
+libraryDependencies ++= Seq(
+  "com.twitter" %% "bijection-core" % "0.9.5",
+  "com.twitter" %% "bijection-util" % "0.9.5"
+)
+
+// finch for web services
+libraryDependencies ++= Seq(
+    "com.twitter" %% "twitter-server" % "18.3.0",
+    "com.github.finagle" %% "finch-core" % "0.17.0",
+    "com.github.finagle" %% "finch-circe" % "0.17.0",
+    "io.circe" %% "circe-generic" % "0.9.0"
 )
--- scala-projects/fb/src/main/scala/com/zenreach/nwservices/CampainCreation.scala
+++ scala-projects/fb/src/main/scala/com/zenreach/nwservices/CampainCreation.scala
@@ -1,8 +1,74 @@
 package com.zenreach.nwservices
 
-import com.facebook.ads.sdk.{APIContext, AdAccount, Campaign}
+import com.twitter.app.Flag
+import com.twitter.conversions.time
+import com.twitter.finagle.{Http, Service}
+import com.twitter.finagle.http.{Request, Response}
+import com.twitter.finagle.param.Stats
+import com.twitter.server.TwitterServer
+import com.twitter.util.{Await, Future}
+import io.finch._
+import io.finch.circe._
+import io.finch.syntax._
+import io.circe.generic.auto._
+import com.twitter.util.Future
 
+//object TestServer extends Service[Request, Response] {
+//
+//  override def apply(req: Request): Future[Response] = message.toServiceAs[Text.Plain].apply(req)
+//
+//  case class Message(message: String)
+//
+//
+//  def currentTime(l: java.util.Locale): String =
+//    java.util.Calendar.getInstance(l).getTime.toString
+//
+//
+//  val message: Endpoint[String] = get("hello") {
+//    Ok("hello")
+//  }
+//}
 
-object CampainCreation extends App {
+case class Message(message: String)
 
+case class FullName(firstName: String, lastName: String)
+
+class ExampleService {
+  val message = Message("Hello, world!")
+
+  def getMessage(): Future[Message] = {
+    Future.value(message)
+  }
+
+  def getFullName(): Future[FullName] =
+    Future.value(FullName("Swapnesh", "Chaubal"))
+}
+
+object CampainCreation extends TwitterServer {
+  val port: Flag[Int] = flag("port", 8081, "TCP port for HTTP server")
+
+  val exampleService = new ExampleService
+
+  def hello: Endpoint[Message] = get("hello") {
+    exampleService.getMessage().map(Ok)
+  }
+
+  def fullName: Endpoint[FullName] = get("fullName") {
+    exampleService.getFullName().map(Ok)
+  }
+
+  val api = (hello :+: fullName).handle {
+    case e: Exception => InternalServerError(e)
+  }
+
+  def main(): Unit = {
+
+    val server = Http.server
+      .withStatsReceiver(statsReceiver)
+      .serve(s":${port()}", api.toServiceAs[Application.Json])
+
+    closeOnExit(server)
+
+    Await.ready(adminHttpServer)
+  }
 }
